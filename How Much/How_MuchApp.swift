0a9417b92d94073e4a66a279a4dddb6d573b2f60
Baris Karalar <b.karalar@jemix.de>
2023-02-21 08:24:46 +0100

    Initial Commit

---
+++ How Much/How_MuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_MuchApp.swift
+//  How Much
+//
+//  Created by Baris Karalar on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct How_MuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
